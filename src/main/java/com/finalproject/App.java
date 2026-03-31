b87bc136707375b8f085c451aa9f598ad91fbf25
Jose Bernardo Carranza Sosa <jcarranzsosa@gmail.com>
2026-03-31 18:50:56 -0500

    Two base structures have been added: Stack and Queue

--- src/main/java/com/finalproject/App.java
+++ src/main/java/com/finalproject/App.java
@@ -1,6 +1,18 @@
 package com.finalproject;
 
+/**
+ * Punto de entrada de la aplicación de ejemplo para el proyecto FinalProject_DS_II.
+ *
+ * <p>Este módulo está diseñado para demostración y pruebas iniciales de la
+ * estructura de la aplicación. En producción, el comportamiento de la aplicación
+ * puede extenderse con controladores, servicios y componentes de UI.
+ */
 public class App {
+  /**
+   * Método principal que arranca la aplicación.
+   *
+   * @param args argumentos de línea de comandos (actualmente no utilizados)
+   */
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
